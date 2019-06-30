2d678284fcd35596549e6d40a780c19904c68ee4
Stefanvdp <51714394+Stefanvdp@users.noreply.github.com>
2019-06-30 20:12:34 +0200

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 %TO DO
-% 1b.Zoek een andere manier om een beschrijving van een biertje te krijgen(kan bij describe), let er ff op dat alles in dezelfde taal moet zijn. Stannnn
+% 1b.Zoek een andere manier om een beschrijving van een biertje te krijgen(kan bij beschrijving), let er ff op dat alles in dezelfde taal moet zijn. Stannnn
 % 2. Voeg rules toe!!!! zou niet weten welke maar zonder rules is het geen kennissysteem!!! keen idee
 % 3. Begin met uitzoeken hoe we een enumeratie kunnen krijgen van alle brouwerijen ipv dat we die zelf handmatig moeten toevoegen aan de answers van de vraag, 
 %    en hoe we ervoor kunnen zorgen dat zodra er geen voorkeur voor is de vraag overgeslagen kan worden, dus dat elk biertje ofwel none of een bepaalde brouwerij heeft.
@@ -18,52 +18,55 @@ intro :-
   write('If you want to go back to the previous question write "go_back(Bier)." in the console.'),nl,
   write('This does not work once a beer has been found, please re enter "main." to start over.'),nl.
 
+%reset all answers for the next run
 reset_answers :-
   retract(progress(_, _)),
   fail.
 reset_answers.
 
+%go back to the previous question
 %it is a known bug that this function cannot be used more than once in each iteration of main because the user can get stuck in a loop of questions.
 %once the questions are going in another order then before you know that there is not a beer that fullfills all requirements.
-
 go_back(Bier) :-
     retract(progress(X, _)),
     bier(Bier),
     outro(Bier).
 
+%find the fitting beer
 find_beer(Bier) :-
   bier(Bier), !.
   :- dynamic(progress/2).
 
+%add description to the beer
 outro(Bier) :-
   write('Based on your answers we propose the following beer:'),nl,nl,
-  describe(Bier),nl,
+  beschrijving(Bier),nl,
   write('If this beer is not really what your looking for type "go_back_(Bier)." to go back one step.'),nl,
   write('Or restart the entire process by typing "main." in the constole.').
 
 parse(0, [H|_], H).
-parse(Index, [H|T], Response) :-
+parse(Index, [H|T], Reactie) :-
   Index > 0,
-  NextIndex is Index - 1,
-  parse(NextIndex, T, Response).
+  VolgendeIndex is Index - 1,
+  parse(VolgendeIndex, T, Reactie).
 
 % creates a list of possible answers
 answers([], _).
 answers([H|T], Index) :-
   write(Index), write(' '), answer(H), nl,
-  NextIndex is Index + 1,
-  answers(T, NextIndex).
+  VolgendeIndex is Index + 1,
+  answers(T, VolgendeIndex).
 
 % Asks the Question to the user and saves the Answer
-ask(Question, Answer, Choices) :-
+stelvraag(Vraag, Antwoord, Keuzes) :-
     nl,
-  question(Question),
+  question(Vraag),
     nl,
-  answers(Choices, 0),
+  answers(Keuzes, 0),
   read(Index),
-  parse(Index, Choices, Response),
-  asserta(progress(Question, Response)),
-  Response = Answer.
+  parse(Index, Keuzes, Reactie),
+  asserta(progress(Vraag, Reactie)),
+  Reactie = Antwoord.
 
 alcohol/1.
 colour/1.
@@ -619,99 +622,98 @@ answer(brewdog):- write('Brewdog').
 answer(apple_bandit):- write('Apple bandit').
 answer(brand):- write('brand brouwerij').
 % Assigns an answer to questions from the knowledge base
-alcohol(Answer) :-
-  progress(alcohol, Answer).
-alcohol(Answer) :-
+alcohol(Antwoord) :-
+  progress(alcohol, Antwoord).
+alcohol(Antwoord) :-
   \+ progress(alcohol, _),
-  ask(alcohol, Answer, [none,low_alcohol,medium_alcohol,high_alcohol]).
-
-colour(Answer) :-
-  progress(colour,Answer).
-colour(Answer) :-
+  stelvraag(alcohol, Antwoord, [none,low_alcohol,medium_alcohol,high_alcohol]).
+colour(Antwoord) :-
+  progress(colour,Antwoord).
+colour(Antwoord) :-
     \+ progress(colour, _),
-  ask(colour, Answer, [light,medium,dark,red]).
-
-clarity(Answer) :-
-  progress(clarity, Answer).
-clarity(Answer) :-
+  stelvraag(colour, Antwoord, [light,medium,dark,red]).
+clarity(Antwoord) :-
+  progress(clarity, Antwoord).
+clarity(Antwoord) :-
   \+ progress(clarity, _),
-  ask(clarity, Answer, [high_transparency,medium_transparency,low_transparency]).
-
-bitterness(Answer):-
-  progress(bitterness,Answer).
-bitterness(Answer) :- 
+  stelvraag(clarity, Antwoord, [high_transparency,medium_transparency,low_transparency]).
+bitterness(Antwoord):-
+  progress(bitterness,Antwoord).
+bitterness(Antwoord) :- 
  \+ progress(bitterness, _),
-  ask(bitterness, Answer, [low,medium,high]).
-
-season(Answer) :-
-  progress(season,Answer).
-season(Answer) :-
+  stelvraag(bitterness, Antwoord, [low,medium,high]).
+season(Antwoord) :-
+  progress(season,Antwoord).
+season(Antwoord) :-
 \+ progress(season, _),
-  ask(season, Answer, [none,winter,spring,summer,autumn]).
-
-region(Answer) :-
-  progress(region,Answer).
-region(Answer) :-
+  stelvraag(season, Antwoord, [none,winter,spring,summer,autumn]).
+region(Antwoord) :-
+  progress(region,Antwoord).
+region(Antwoord) :-
 \+ progress(region, _),
-  ask(region, Answer, [holland,belgium,ireland,france,california,mexico,england]).
-
-foam(Answer) :-
- progress(foam,Answer).
-foam(Answer) :-
+  stelvraag(region, Antwoord, [holland,belgium,ireland,france,california,mexico,england]).
+foam(Antwoord) :-
+ progress(foam,Antwoord).
+foam(Antwoord) :-
 \+ progress(foam, _),
- ask(foam, Answer, [low,medium,high]).
-
-taste(Answer) :-
-  progress(taste,Answer).
-taste(Answer) :-
+ stelvraag(foam, Antwoord, [low,medium,high]).
+taste(Antwoord) :-
+  progress(taste,Antwoord).
+taste(Antwoord) :-
 \+ progress(taste, _),
-  ask(taste, Answer, [bitter,fruity,refreshing,sweet]).
-
-brewery(Answer) :-
-progress(brewery,Answer).
-brewery(Answer) :-
+  stelvraag(taste, Antwoord, [bitter,fruity,refreshing,sweet]).
+brewery(Antwoord) :-
+progress(brewery,Antwoord).
+brewery(Antwoord) :-
 \+ progress(brewery, _),
-  ask(brewery, Answer,[achel,adnams,amstel,anchor,apple_bandit,bavaria_brewery,bax,boon,bosteels,brand,brewdog,brouwerij_t_IJ,chimay,corona,de_klok_brewery,duvel_brewery,grolsch_brewery,guinness_brewery,gulpener,heineken_brewery,hertog_jan_brewery,kasteel,lindemans,neude_bier,palm,schutzenberger,straffe_hendrik,vedett]).
+  stelvraag(brewery, Antwoord,[achel,adnams,amstel,anchor,apple_bandit,bavaria_brewery,bax,boon,bosteels,brand,brewdog,brouwerij_t_IJ,chimay,corona,de_klok_brewery,duvel_brewery,grolsch_brewery,guinness_brewery,gulpener,heineken_brewery,hertog_jan_brewery,kasteel,lindemans,neude_bier,palm,schutzenberger,straffe_hendrik,vedett]).
 
 %descriptions
-describe(heineken_pils):- write('Heineken pils'),nl.
-describe(heineken_pils_0) :- write('heineken pils 0.0'),nl.
-describe(grolsch_herfstbok):- write('Grolsch herfstbok'),nl.
-describe(grolsch_lentebok):- write('Grolsch lentebok'),nl.
-describe(hertog_jan_lentebock):- write('Hertog Jan lentebok'),nl.
-describe(hertog_jan_pils):- write('Hertog Jan pils')
-describe(grolsch_kanon):- write('Grolsch kanon'),nl.
-describe(guinness_ale):- write('Guinnes ale'),nl.
-describe(de_klok_pils):- write('De klok bier'),nl.
-describe(duvel):- write('Duvel'),nl.
-describe(lindemans_kriek):- write('Lindemans kriek'),nl.
-describe(neude_wit):- write('Neude wit'),nl.
-describe(kwak):- write('Pauwel kwak'),nl.
-describe(amstel_radler):- write('Amstel radler'),nl.
-describe(hertog_jan_weizener):- write('Hertog Jan weizener'),nl.
-describe(bavaria_pils):- write('Bavaria pils'),nl.
-describe(achel_extra_blond):- write('Achel extra blond'),nl.
-describe(achel_extra_bruin):- write('Achel extra bruin'),nl.
-describe(old_ale):- write('Old ale'),nl.
-describe(schutzenberger_bieres_de_noel):- write('Bières de noël'),nl.
-describe(anchor_porter):- write('Anchor porter'),nl.
-describe(straffe_hendrik_quadrupel):- write('Straffe hendrik quadrupel'),nl.
-describe(speciale_palm):- write('Speciale palm'),nl.
-describe(desperados):- write('Desperados'),nl.
-describe(corona):- write('Corona'),nl.
-describe(boon_geuze):- write('Boom geuze'),nl.
-describe(vedett_extra_white):- write('Vedett extra white'),nl.
-describe(kasteel_donker):- write('Kasteel donker'),nl.
-describe(chimay_blauw):- write('Chimay blauw'),nl.
-describe(chimay_tripel_wit):- write('Chimay tripel wit'),nl.
-describe(ij_natte):- write('IJ natte'),nl.
-describe(bax_rokkenjager):- write('Bax rokkenjager'),nl.
-describe(gulpener_ijsbock):- write('Gulpener ijsbock'),nl.
-describe(brewdog_dead_pony_club):- write('Brewdog dead pony club'),nl.
-describe(brand_session_india_pale_ale):- write('Brand session indiana pale ale'),nl. 
-describe(punk_ipa_pale_ale) :- write('Punk ipa pale ale'),nl.
-describe(guinness_stout_original):- write('Guinness stout original'),nl.
-describe(apple_bandit_cider_crisp_apple):- write('Apple bandit cider crisp apple'),nl. 
+
+beschrijving(heineken_pils):- write('Heineken pils'),nl,write('A famous beer from brewery Heineken. This beer has an alcohol percentage of 5% and has a fresh bitter taste.'),nl.
+beschrijving(heineken_pils_0) :- write('heineken pils 0.0'),nl,write('The alcohol-free brother of Heineken pils. This beer tastes bitter andfresh with a soft malty body.'),nl.
+beschrijving(grolsch_herfstbok):- write('Grolsch herfstbok'),nl,write('This season beer with 6,5% alcohol tastes bitter and hints of caramel and is a perfect drink for Autumn.'),nl.
+beschrijving(grolsch_lentebok):- write('Grolsch lentebok'),nl,write('Best drank in Spring. This gold-yellow beer has a fruity, sweet taste and strong aftertaste.'),nl.
+beschrijving(hertog_jan_lentebock):- write('Hertog Jan lentebok'),nl,write('This beer by Hertog Jan is best enjoyed in Spring. This gold-yellow beer has a fruity, sweet taste and strong aftertaste.'),nl.
+beschrijving(hertog_jan_pils):- write('Hertog Jan pils'),nl,write('A bitter and tasty beer by Hertog Jan. Has an alcohol percentage of 5% and is a solid beverage for all occassions.'),nl.
+beschrijving(grolsch_kanon):- write('Grolsch kanon'),nl,write('A very strong beer with an alcohol percentage of 11,6%. It is a blond beer with a strong sweet taste.'),nl.
+beschrijving(guinness_ale):- write('Guinness ale'),nl,write('A dark beer brewed in Ireland. Has an alcohol percentage of 5% and an strong bitter taste.'),nl.
+beschrijving(de_klok_pils):- write('De klok bier'),nl,write('Well... its something :('),nl.
+beschrijving(duvel):- write('Duvel'),nl,write('A belgium classic. This light-bitter blond beer has an alcohol percentage of 8,5%, the brewing process takes about 90 days.'),nl.
+beschrijving(lindemans_kriek):- write('Lindemans kriek'),nl,write('Lindemans Kriek is a kriek beer which means it has a sweet, fruity tast. It is best served with ice to cool down from good weather.'),nl.
+beschrijving(neude_wit):- write('Neude wit'),nl,write('Best enjoyed whilst discussing korfball'),nl.
+beschrijving(kwak):- write('Pauwel kwak'),nl,write('Pauwel kwak is a strong beer with an alcohol percentage of 8,4%. It is a sweat oaky accent and bitter aftertaste served in an iconic glass.'),nl.
+beschrijving(amstel_radler):- write('Amstel radler'),nl,write('Amstel Radler is a light-alcoholic, very refreshing beverage. A mix of Amstel beer and lemonwater, this is best enjoyed in the summer.'),nl.
+beschrijving(hertog_jan_weizener):- write('Hertog Jan weizener'),nl,write('Hertog Jan Weizener is a full, strong beer with an alcohol percentage of 5,7%.'),nl.
+beschrijving(bavaria_pils):- write('Bavaria pils'),nl,write('This beer from the Bavaria brewery is very refreshing with a bitter aftertaste.'),nl.
+beschrijving(achel_extra_blond):- write('Achel extra blond'),nl,write('This extra blond beer from Achel has a high alcohol-percentage and is very refreshing with a sweet aftertaste.'),nl.
+beschrijving(achel_extra_bruin):- write('Achel extra bruin'),nl,write('A counterpart from Achel extra blond, this brown beer has a high-alcohol percentage, is bitter and has hints of oak.'),nl.
+beschrijving(adnams_old_ale):- write('Adnams old ale'),nl,write('This beer is only brewed for cunsumption during the winter season. It is an English beer with a bitter taste and medium alcohol content.'),nl.
+beschrijving(schutzenberger_bieres_de_noel):- write('Schutzenberge bières de noël'),nl,write('A French winter warmer, this dark beer has a high alcohol percentage and is bitter with a sweet aftertaste.'),nl.
+beschrijving(anchor_porter):- write('Anchor porter'),nl,write('A sweet English winter warmer which is a great addition when drank with meat as a meal. Has 5% alcohol.'),nl.
+beschrijving(straffe_hendrik_quadrupel):- write('Straffe hendrik quadrupel'),nl,write('This queadrupel beer has a whopping 11% alcohol content. It is a rich, dark beer witha complex character and taste.'),nl.
+beschrijving(speciale_palm):- write('Speciale palm'),nl,write('This amber beer is a honey-tasting belgium beer with fruity accents. It has an alcohol percentage of 5,4%.'),nl.
+beschrijving(desperados):- write('Desperados'),nl,write('A popular Mexican beer. Desperados is a true summer beer. Very refreshing as it is enriched with the flavor of Tequila. A good party beverage'),nl.
+beschrijving(corona):- write('Corona'),nl,write('Corona is an excellent summer beverage made on Mexican soil. It has an alcohol percentage of 4,6%. Featured in Fast & Furious a lot.'),nl.
+beschrijving(boon_geuze):- write('Boom geuze'),nl,write('A fruity kriek beer. With an alcohol percentage of 7%, it is a unique and rich beer.'),nl.
+beschrijving(vedett_extra_white):- write('Vedett extra white'),nl,write('A very light and refreshing beer which taskes good next ot chicken meals. Has medium alcohol contents. '),nl.
+beschrijving(kasteel_donker):- write('Kasteel donker'),nl,write('Heineken pils'),nl.
+beschrijving(chimay_blauw):- write('Chimay blauw'),nl,write('Heineken pils'),nl.
+beschrijving(chimay_tripel_wit):- write('Chimay tripel wit'),nl,write('Heineken pils'),nl.
+beschrijving(ij_natte):- write('IJ natte'),nl,write('Heineken pils'),nl.
+beschrijving(bax_rokkenjager):- write('Bax rokkenjager'),nl,write('Heineken pils'),nl.
+beschrijving(gulpener_ijsbock):- write('Gulpener ijsbock'),nl,write('Heineken pils'),nl.
+beschrijving(brewdog_dead_pony_club):- write('Brewdog dead pony club'),nl,write('Heineken pils'),nl.
+beschrijving(brand_session_india_pale_ale):- write('Brand session indiana pale ale'),nl,write('Heineken pils'),nl.
+beschrijving(punk_ipa_pale_ale) :- write('Punk ipa pale ale'),nl,write('Heineken pils'),nl.
+beschrijving(guinness_stout_original):- write('Guinness stout original'),nl,write('Heineken pils'),nl.
+beschrijving(apple_bandit_cider_crisp_apple):- write('Apple bandit cider crisp apple'),nl,write('Very sweet and refreshing cider with an iconic fox as the icon.'),nl.
+beschrijving(bavaria_radler):- write('Bavaria radler'),nl.
+beschrijving(duvel_triple_hop):- write('Duvel triple hop'),nl.
+beschrijving(amstel_pils):- write('Amstel pils'),nl.
+beschrijving(grolsch_radler):- write('Grolsch radler'),nl.
+beschrijving(heineken_radler):- write('Heineken radler'),nl.
+
 
 
 
